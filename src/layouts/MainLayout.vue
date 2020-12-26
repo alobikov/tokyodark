6312fa8b153688d28d799732433f2b7bb4a98ea1
Aleksej Lobikov <alex.lobikov@tenging.is>
2020-12-26 18:33:46 +0200

    working on landing page in progress

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,37 +1,27 @@
 <template>
   <q-layout view="hHr lpR fFr">
-    <q-header class="bg-primary text-white" height-hint="98">
-      <q-toolbar>
-        <q-toolbar-title class="header__title">
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" />
-          </q-avatar>
-          Title
-        </q-toolbar-title>
-
-        <q-btn dense flat round icon="menu" @click="right = !right" />
-      </q-toolbar>
-
-      <q-tabs align="left">
+    <!-- <q-header class="bg-primary text-white" height-hint="98">
+      <q-tabs align="left" class="toptab">
         <q-route-tab to="/page1" label="Page One" />
         <q-route-tab to="/page2" label="Page Two" />
         <q-route-tab to="/page3" label="Page Three" />
       </q-tabs>
-    </q-header>
+    </q-header> -->
 
-    
-      <q-drawer
-        :value="true"
-        show-if-above
-        side="right"
-        :mini="right"
-
-        :width="300"
-        :breakpoint="500"
-        bordered
-        content-class="bg-grey-3"
-      >
-     <h3>This is drawer</h3>
+    <q-drawer
+      side="right"
+      v-model="drawer"
+      show-if-above
+      :mini="miniState"
+      @mouseover="miniState = false"
+      @mouseout="miniState = true"
+      mini-to-overlay
+      :width="200"
+      :breakpoint="500"
+      bordered
+      content-class="bg-grey-3"
+    >
+      <h3>This is drawer</h3>
     </q-drawer>
 
     <q-page-container>
@@ -50,73 +40,74 @@
     </q-footer>
   </q-layout>
 </template>
-<style land='scss'>
+<style land="scss">
 .header__title {
   width: 40%;
   flex: 0 1 100px;
 }
 h3 {
-  writing-mode: vertical-rl; text-orientation: upright;
-margin:0;
-
+  writing-mode: vertical-rl;
+  text-orientation: upright;
+  margin: 0;
 }
-
+.toptab {
+  background-color: rgba(0, 0, 0, 0);
+}
 </style>
 
 <script>
 const linksData = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev"
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework"
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev"
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev"
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev"
   },
   {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev"
   },
   {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev"
   }
 ];
 
 export default {
-  name: 'MainLayout',
-  data () {
+  name: "MainLayout",
+  data() {
     return {
-      leftDrawerOpen: false,
-      essentialLinks: linksData,
-      right: false,
-    }
+      drawer: false,
+      miniState: true
+    };
   }
-}
+};
 </script>
---
+++ src/pages/Landing.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="background">Test</q-page>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.background {
+  background-color: black;
+  background-image: url("~assets/Main Image@1x.png");
+  background-size: content;
+  background-repeat: no-repeat;
+  background-position-x: center;
+  background-position-y: center;
+}
+.q-page-container {
+  /* padding-top: 0px !important; */
+  padding-bottom: 0px !important;
+  min-height: 100% !important;
+}
+main {
+  height: 100% !important;
+}
+</style>
